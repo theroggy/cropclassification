575299252475669567946e147936a62a59e72572
theroggy <pieter.roggemans@gmail.com>
2019-04-23 17:23:30 +0200

    Added some examples as test data

--- marker/marker_multicrop.py
+++ marker/marker_multicrop.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 15:51:26 2018
+
+@author: pierog
+"""
+
+"""
+Testdata in 2018: Parcels with 2 crops in them:
+"00002806653D6B97"   # 10%/90% twee gewassen
+"00002806653D6ACD"   # 50%/50% twee gewassen
+"0000280664E917CC"   # Verwaarloosbaar stukje in perceel dat er wat anders uit ziet
+"""
+
 import sys
 [sys.path.append(i) for i in ['.', '..']]
 
