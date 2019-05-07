f1fc614b20fb5668421ed99cbfe0222c40b323c6
theroggy <pieter.roggemans@gmail.com>
2019-05-07 13:08:30 +0200

    Make code easier to read

--- cropclassification/helpers/dir_helper.py
+++ cropclassification/helpers/dir_helper.py
@@ -1,29 +1,25 @@
 import os
 
-def create_dir(class_base_dir: str, 
-               reuse_last_run_dir: bool):
+def create_run_dir(class_base_dir: str, 
+                   reuse_last_run_dir: bool):
                
     max_run_dir_id = 998
     prev_class_dir = None
     class_dir = None 
-    for i in range(max_run_dir_id):
+    for i in range(1, max_run_dir_id):
         # Check if we don't have too many run dirs for creating the dir name
         if i >= max_run_dir_id:
             raise Exception("Please cleanup the run dirs, too many!!!")
             
         # Now search for the last dir that is in use
-        class_dir = os.path.join(class_base_dir, f"Run_{i+1:03d}")
+        class_dir = os.path.join(class_base_dir, f"Run_{i:03d}")
         if os.path.exists(class_dir):
             prev_class_dir = class_dir
             continue
         else:
             # If we want to reuse the last dir, do so...
-            if reuse_last_run_dir and prev_class_dir is not None:
-                class_dir = prev_class_dir
-                break         
+            if reuse_last_run_dir is True and prev_class_dir is not None:
+                return prev_class_dir
             else:
-                # Otherwise create new dir name with next index
-                class_dir = os.path.join(class_base_dir, f"Run_{i+1:03d}")         
-                break 
-
-    return class_dir
+                # Otherwise return the newest dir
+                return class_dir
