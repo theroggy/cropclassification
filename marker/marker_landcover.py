a04302cc9052d8b9bd5989010092c36a08dfc610
theroggy <pieter.roggemans@gmail.com>
2019-05-08 12:19:04 +0200

    Add option to choose whether to reuse last run dir using a parameter instead of via config

--- marker/marker_landcover.py
+++ marker/marker_landcover.py
@@ -6,7 +6,8 @@ import cropclassification.marker_runner as runner
 def main():        
     runner.run(config_filepaths=['marker/general.ini',
                                  'marker/marker_landcover.ini',
-                                 'marker/local_overrule.ini'])
+                                 'marker/local_overrule.ini'],
+               reuse_last_run_dir=False)
     
 if __name__ == '__main__':
     main()
