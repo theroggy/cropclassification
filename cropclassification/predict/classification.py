bfceb410c5463fe847cdabc4cead13dd8262914a
theroggy <pieter.roggemans@gmail.com>
2019-05-14 22:33:17 +0200

    Add prediction_withdoubt_column to the classification output, to be able to improve reporting
    + small improvements to comments

--- cropclassification/predict/classification.py
+++ cropclassification/predict/classification.py
@@ -59,7 +59,7 @@ def train_test_predict(input_parcel_train_csv: str,
     df_input_parcel_classification_data.set_index(conf.csv['id_column'], inplace=True)
     logger.debug('Read classification data file ready')
 
-    # Train the classifiaction
+    # Train the classification
     train(input_parcel_train_csv=input_parcel_train_csv,
           input_parcel_classification_data_csv=input_parcel_classification_data_csv,
           output_classifier_filepath=output_classifier_filepath,
@@ -154,18 +154,19 @@ def predict(input_parcel_csv: str,
     df_top3 = _get_top_3_prediction(df_proba)
 
     # Add the consolidated prediction
+    # TODO: try to rewrite using native pandas commands to improve performance
     def calculate_consolidated_prediction(row):
         # For some reason the row['pred2_prob'] is sometimes seen as string, and so 2* gives a
         # repetition of the string value instead of a mathematic multiplication... so cast to float!
 
-# float(row['pred1_prob']) > 30
+        # float(row['pred1_prob']) > 30
         if ((float(row['pred1_prob']) >= 2.0 * float(row['pred2_prob']))):
             return row[conf.csv['prediction_column']]
         else:
             return 'DOUBT'
 
     values = df_top3.apply(calculate_consolidated_prediction, axis=1)
-    df_top3.insert(loc=2, column=conf.csv['prediction_cons_column'], value=values)
+    df_top3.insert(loc=2, column=conf.csv['prediction_withdoubt_column'], value=values)
 
     # Make sure all input parcels are in the output. If there was no prediction, it means that there
     # was no data available for a classification, so set prediction to NODATA
@@ -174,24 +175,31 @@ def predict(input_parcel_csv: str,
 
     # Add a column with the prediction status... and all parcels in df_top3 got a prediction
     df_top3[conf.csv['prediction_status']] = 'OK'
-    df_top3.loc[(df_top3[conf.csv['prediction_cons_column']] == 'DOUBT'),
+    df_top3.loc[(df_top3[conf.csv['prediction_withdoubt_column']] == 'DOUBT'),
                 conf.csv['prediction_status']] = 'DOUBT'
 
     cols_to_join = df_top3.columns.difference(df_input_parcel.columns)
     df_pred = df_input_parcel.join(df_top3[cols_to_join], how='left')
     df_pred[conf.csv['prediction_column']].fillna('NODATA', inplace=True)
-    df_pred[conf.csv['prediction_cons_column']].fillna('NODATA', inplace=True)
+    df_pred[conf.csv['prediction_withdoubt_column']].fillna('NODATA', inplace=True)
     df_pred[conf.csv['prediction_status']].fillna('NODATA', inplace=True)
 
     logger.info(f"Columns of df_pred: {df_pred.columns}")
-    # Parcels with too few pixels don't have a good accuracy and give many alfa errors...
+
+    # Now calculate the full consolidated prediction: 
+    #    * Can be doubt if probability too low
+    #    * Parcels with too few pixels don't have a good accuracy and give many alfa errors...
+    df_pred[conf.csv['prediction_cons_column']] = df_pred[conf.csv['prediction_withdoubt_column']]
     df_pred.loc[(df_pred[conf.csv['pixcount_s1s2_column']] <= conf.marker.getint('min_nb_pixels'))
                  & (df_pred[conf.csv['prediction_status']] != 'NODATA')
                  & (df_pred[conf.csv['prediction_status']] != 'DOUBT'),
                 [conf.csv['prediction_cons_column'], conf.csv['prediction_status']]] = 'NOT_ENOUGH_PIXELS'
 
-    df_pred.loc[df_pred[conf.csv['class_column']].isin(conf.marker.getlist('classes_to_ignore_for_train')), [conf.csv['prediction_status']]] = 'UNKNOWN'
-    df_pred.loc[df_pred[conf.csv['class_column']].isin(conf.marker.getlist('classes_to_ignore')), [conf.csv['prediction_status']]] = df_pred[conf.csv['class_column']]
+    # Set the prediction status for classes that should be ignored
+    df_pred.loc[df_pred[conf.csv['class_column']].isin(conf.marker.getlist('classes_to_ignore_for_train')), 
+                [conf.csv['prediction_status']]] = 'UNKNOWN'
+    df_pred.loc[df_pred[conf.csv['class_column']].isin(conf.marker.getlist('classes_to_ignore')), 
+                [conf.csv['prediction_status']]] = df_pred[conf.csv['class_column']]
 
     logger.info("Write final prediction data to file")
     df_pred.to_csv(output_predictions_csv, float_format='%.10f', encoding='utf-8')
@@ -200,11 +208,11 @@ def _get_top_3_prediction(df_probabilities):
     """ Returns the top 3 predictions for each parcel.
 
     The return value will be a dataset with the following columns:
-        - global_settings.id_column: id of the parcel as in the input
-        - global_settings.class_column: class of the parcel as in the input
-        - global_settings.prediction_cons_column: the consolidated prediction, will be 'DOUBT'
+        - id_column: id of the parcel as in the input
+        - class_column: class of the parcel as in the input
+        - prediction_cons_column: the consolidated prediction, will be 'DOUBT'
           if the prediction had a relatively low probability.
-        - global_settings.prediction_columns: prediction with the highest probability
+        - prediction_columns: prediction with the highest probability
         - pred1_prob: probability of the best prediction
         - pred2
         - pred2_prob
@@ -235,7 +243,7 @@ def _get_top_3_prediction(df_probabilities):
     id_class_top3 = np.concatenate([df_probabilities[[conf.csv['id_column'], conf.csv['class_column']]].values, top3_pred]
                                    , axis=1)
 
-    # Convert to dataframe, combine with input data and write to file
+    # Convert to dataframe and return
     df_top3 = pd.DataFrame(id_class_top3,
                            columns=[conf.csv['id_column'], conf.csv['class_column'],
                                     conf.csv['prediction_column'], 'pred2', 'pred3',
