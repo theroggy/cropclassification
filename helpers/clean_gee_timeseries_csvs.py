ce7d7e77ed2f2589bafe63686fee92dd1cc057b6
theroggy <pieter.roggemans@gmail.com>
2019-05-29 21:10:46 +0200

    Add year parameter

--- helpers/clean_gee_timeseries_csvs.py
+++ helpers/clean_gee_timeseries_csvs.py
@@ -10,8 +10,7 @@ import cropclassification.preprocess.timeseries_calc_gee as ts_calc_gee
 
 def main():        
     # Read the configuration
-    conf.read_config(['config/general.ini',
-                      'config/local_overrule.ini'])
+    conf.read_config(['config/general.ini', 'config/local_overrule.ini'], year=None)
 
     # Go!
     imagedata_dir = conf.dirs['imagedata_dir']
