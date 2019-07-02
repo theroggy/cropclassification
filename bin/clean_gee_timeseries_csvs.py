649f1fb3b7eebe1f0878469957a19a5f1dc99744
theroggy <pieter.roggemans@gmail.com>
2019-07-03 01:04:18 +0200

    Use new paths to timeseries data

--- bin/clean_gee_timeseries_csvs.py
+++ bin/clean_gee_timeseries_csvs.py
@@ -10,11 +10,12 @@ import cropclassification.preprocess.timeseries_calc_gee as ts_calc_gee
 
 def main():        
     # Read the configuration
-    conf.read_config(['config/general.ini', 'config/local_overrule.ini'], year=None)
+    conf.read_config(['config/general.ini', 'config/local_overrule.ini'], 
+                     year=None)
 
     # Go!
-    imagedata_dir = conf.dirs['imagedata_dir']
-    ts_calc_gee.clean_gee_downloaded_csvs_in_dir(imagedata_dir)
+    timeseries_periodic_dir = conf.dirs['timeseries_periodic_dir']
+    ts_calc_gee.clean_gee_downloaded_csvs_in_dir(timeseries_periodic_dir)
     
 if __name__ == '__main__':
     main()
