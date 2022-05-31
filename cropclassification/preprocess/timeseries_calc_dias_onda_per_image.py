0b9b747316c94329f9288717bd9fca3a1aea1d8a
Pieter Roggemans <pieter.roggemans@gmail.com>
2022-05-31 09:15:16 +0200

    Fix file lock issue (on windows) (#11)

--- cropclassification/preprocess/timeseries_calc_dias_onda_per_image.py
+++ cropclassification/preprocess/timeseries_calc_dias_onda_per_image.py
@@ -1313,17 +1313,22 @@ def projected_bounds_to_window(
 
     return window_to_read
 
-def create_file_atomic(filename: Path):
+def create_file_atomic(path: Path):
     """
     Create a lock file in an atomic way, so it is threadsafe.
 
     Returns True if the file was created by this thread, False if the file existed already.
     """
+    fd = None
     try:
-        os.open(filename,  os.O_CREAT | os.O_EXCL)
+        fd = os.open(path,  os.O_CREAT | os.O_EXCL)
         return True
     except FileExistsError:
         return False
+    finally:
+        if fd is not None:
+            os.close(fd)
+
 
 def prepare_image(
         image_path: Path,
