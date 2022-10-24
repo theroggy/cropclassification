e389d87819c9c1ada780c57ee8a82e7f8b33e113
theroggy <pieter.roggemans@gmail.com>
2022-10-24 15:01:27 +0200

    Reduce memory usage of multiprocess zonal_stats

--- cropclassification/helpers/geoops_util.py
+++ cropclassification/helpers/geoops_util.py
@@ -5,12 +5,14 @@ import multiprocessing
 import os
 from pathlib import Path
 import shutil
+from time import time
 from typing import Optional
 
 import geofileops as gfo
 import geopandas as gpd
 from geofileops.util import _io_util
 import psutil
+from psutil._common import bytes2human
 import rasterstats
 
 logger = logging.getLogger()
@@ -53,7 +55,7 @@ def zonal_stats(
     nb_per_batch = int(info.featurecount / nb_batches)
 
     nb_parallel = multiprocessing.cpu_count()
-    future_to_batch_id = {}
+    future_infos = {}
 
     logger.info(
         f"calculate zonal_stats for {input_vector_path.name}, in {nb_batches} batches, "
@@ -72,10 +74,14 @@ def zonal_stats(
                 else:
                     rows = slice(batch_start)
 
+                output_temp_partial_path = (
+                    output_temp_path.parent / f"{output_temp_path.stem}_{batch_id}.gpkg"
+                )
                 future = calculate_pool.submit(
                     zonal_stats_ext,
                     vector_path=input_vector_path,
                     raster_path=input_raster_path,
+                    output_path=output_temp_partial_path,
                     layer=input_vector_layer,
                     band=band,
                     nodata=nodata,
@@ -92,26 +98,44 @@ def zonal_stats(
                     boundless=boundless,
                     rows=rows,
                 )
-                future_to_batch_id[future] = batch_id
+                future_infos[future] = {
+                    "batch_id": batch_id,
+                    "output_temp_partial_path": output_temp_partial_path,
+                }
                 batch_start += nb_per_batch
 
             # Loop till all parallel processes are ready, but process each one
             # that is ready already
             batch_id = None
-            for future in futures.as_completed(future_to_batch_id):
+            for future in futures.as_completed(future_infos):
                 try:
-                    batch_id = future_to_batch_id[future]
-                    result_gdf = future.result()
+                    future_info = future_infos[future]
+                    batch_id = future_info["batch_id"]
+                    output_temp_partial_path = future_info["output_temp_partial_path"]
+                    _ = future.result()
 
                     # If the calculate gave results, copy to output
-                    logger.info(f"Resultaat wegschrijven voor batch {batch_id}")
-                    result_gdf = gpd.GeoDataFrame.from_features(result_gdf)
-                    gfo.to_file(result_gdf, output_temp_path, append=True)
+                    if (
+                        output_temp_partial_path.exists()
+                        and output_temp_partial_path.stat().st_size > 0
+                    ):
+                        logger.info(f"Write result for batch {future_info['batch_id']}")
+                        gfo.append_to(
+                            src=output_temp_partial_path,
+                            dst=output_temp_path,
+                        )
+                        gfo.remove(output_temp_partial_path)
 
                 except Exception:
                     # calculate_pool.shutdown()
                     logger.exception(f"Error executing {batch_id}")
                     raise
+
+                logger.info(
+                    "Available virtual memory: "
+                    f"{bytes2human(psutil.virtual_memory().available)}, "
+                    f"pct virtual memory used: {psutil.virtual_memory().percent}"
+                )
 
             # We are ready, so move the result to the permanent location
             if output_temp_path.exists():
@@ -146,6 +170,7 @@ def process_nice_to_priority_class(nice_value: int) -> int:
 def zonal_stats_ext(
     vector_path: Path,
     raster_path: Path,
+    output_path: Path,
     layer: Optional[str] = None,
     band: int = 1,
     nodata=None,
@@ -161,13 +186,18 @@ def zonal_stats_ext(
     geojson_out: bool = False,
     boundless: bool = True,
     rows: Optional[slice] = None,
-):
+) -> bool:
+    # If there isn't enough memory available (1 GB), sleep a while
+    while True:
+        if psutil.virtual_memory().available > 1000000000:
+            break
+        time.sleep(1)
 
-    # Filter the rows we want to process in this batch
+    # Read the rows we want to process in this batch
     vector_gdf = gfo.read_file(path=vector_path, layer=layer, rows=rows)
 
     # Calculate
-    return rasterstats.zonal_stats(
+    result = rasterstats.zonal_stats(
         vectors=vector_gdf,
         raster=raster_path,
         band=band,
@@ -184,3 +214,7 @@ def zonal_stats_ext(
         geojson_out=geojson_out,
         boundless=boundless,
     )
+    result_gdf = gpd.GeoDataFrame.from_features(result)
+    gfo.to_file(result_gdf, output_path)
+
+    return True
