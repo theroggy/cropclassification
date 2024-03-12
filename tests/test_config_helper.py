a24adf9a7d7d9ebd018c12587322847f70259d30
Kris Van Wayenberge <Kris.VanWayenberge@lv.vlaanderen.be>
2024-03-12 13:20:54 +0100

    - changed config_path for test_get_image_profiles

--- tests/test_config_helper.py
+++ tests/test_config_helper.py
@@ -18,7 +18,6 @@ from cropclassification.helpers import config_helper as conf
 def test_get_image_profiles(sensor: str):
     data_dir = Path(__file__).resolve().parent / "data"
     config_path = data_dir / "image_profiles.ini"
-    config_path = conf.marker.getpath("image_profiles_config_filepath")
     image_profiles = conf._get_image_profiles(config_path)
 
     profile = image_profiles.get(sensor)
