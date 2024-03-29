593859354dbdeb0191c8a30b98f279a32139549b
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-03-29 14:22:34 +0100

    Rename sample dir

--- tests/test_config_helper.py
+++ tests/test_config_helper.py
@@ -2,10 +2,7 @@ from pathlib import Path
 import pytest
 
 from cropclassification.helpers import config_helper as conf
-
-sampleprojects_dir = Path(__file__).resolve().parent.parent / "sample_projects"
-project_dir = sampleprojects_dir / "markers"
-config_dir = project_dir / "_config"
+from tests import test_helper
 
 
 @pytest.mark.parametrize(
@@ -20,7 +17,7 @@ config_dir = project_dir / "_config"
     ],
 )
 def test_get_image_profiles(sensor: str):
-    config_path = config_dir / "image_profiles.ini"
+    config_path = test_helper.SampleDirs.config_dir / "image_profiles.ini"
     image_profiles = conf._get_image_profiles(config_path)
 
     profile = image_profiles.get(sensor)
--- tests/test_cropclassification_tasks.py
+++ tests/test_cropclassification_tasks.py
@@ -1,18 +1,18 @@
 import shutil
-from tests import test_helper
 
 from cropclassification import cropclassification
+from tests import test_helper
 
 
 def test_task_calc_periodic_mosaic(tmp_path):
-    projects_dir = tmp_path / test_helper.sampleprojects_dir.name
-    shutil.copytree(test_helper.sampleprojects_dir, projects_dir)
+    marker_basedir = tmp_path / test_helper.SampleDirs.marker_basedir.name
+    shutil.copytree(test_helper.SampleDirs.marker_basedir, marker_basedir)
 
     # Create configparser and read task file!
-    tasks_dir = projects_dir / "_tasks"
+    tasks_dir = marker_basedir / "_tasks"
     cropclassification.cropclassification(tasksdir=tasks_dir)
 
     # Check if a log file was written
-    log_dir = projects_dir / "log"
+    log_dir = marker_basedir / "log"
     assert log_dir.exists()
     assert len(list(log_dir.glob("*.log"))) == 1
