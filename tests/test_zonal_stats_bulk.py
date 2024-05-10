d6c8364948a1ed26d7db589f52db172a06d1eaed
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-05-10 14:49:19 +0200

    Fix test_zonal_stats_bulk

--- tests/test_zonal_stats_bulk.py
+++ tests/test_zonal_stats_bulk.py
@@ -15,10 +15,13 @@ def test_zonal_stats_bulk(tmp_path, engine):
     test_dir = tmp_path / sample_dir.name
     shutil.copytree(sample_dir, test_dir)
 
-    image_dir = test_dir / "periodic_mosaic/roi_test"
-    input_image_paths = list((image_dir / "s1-grd-sigma0-asc").glob("*.tif"))
-    input_image_paths.extend((image_dir / "s1-grd-sigma0-desc").glob("*.tif"))
-    images_bands = [(path, ["VV", "VH"]) for path in input_image_paths]
+    # Make sure the s2-agri input file was copied
+    test_image_roi_dir = test_dir / SampleData.image_dir.name / SampleData.roi_name
+    test_s1_asc_dir = test_image_roi_dir / SampleData.image_s1_asc_path.parent.name
+    test_s1_desc_dir = test_image_roi_dir / SampleData.image_s1_desc_path.parent.name
+    test_image_paths = list(test_s1_asc_dir.glob("*.tif"))
+    test_image_paths.extend(test_s1_desc_dir.glob("*.tif"))
+    images_bands = [(path, ["VV", "VH"]) for path in test_image_paths]
     vector_path = test_dir / SampleData.input_dir.name / "Prc_BEFL_2023_2023-07-24.gpkg"
     vector_info = gfo.get_layerinfo(vector_path)
 
