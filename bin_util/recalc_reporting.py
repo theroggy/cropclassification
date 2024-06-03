60a399af1d5dbdf0bf9105b663966eba192b2f51
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-06-03 17:26:17 +0200

    ENH: broaden recalculation in recalc_reporting script (#106)

--- bin_util/recalc_reporting.py
+++ bin_util/recalc_reporting.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 
 import cropclassification.helpers.config_helper as conf
+from cropclassification.postprocess import classification_postprocess as class_post
 from cropclassification.preprocess import classification_preprocess as class_pre
 from cropclassification.postprocess import classification_reporting as class_report
 
@@ -13,23 +14,28 @@ def main():
 
     input_groundtruth_filename = "Prc_BEFL_2022_2024_05_13_groundtruth.tsv"
     run_dir = Path(
-        "X:/Monitoring/Markers/dev/2022_CROPGROUP/Run_2022-08-02_001_groundtruth"
+        "X:/Monitoring/Markers/dev/2022_CROPGROUP/Run_2022-08-02_001_91.63_groundtruth"
     )
     basedir = run_dir.parent.parent
     overrules = [
         "columns.class_declared2=NOT_AVAILABLE",
         "marker.roi_name=BEFL",
+        # "calc_marker_params.country_code=BEFL",
     ]
     """
     input_groundtruth_filename = "Prc_BEFL_2022_2024_05_13_groundtruth.tsv"
     run_dir = Path(
-        "X:/Monitoring/Markers/dev/2022_CROPGROUP/Run_2024-05-17_001_96.07_min80pct"
+        "X:/Monitoring/Markers/dev/2022_CROPGROUP/Run_2024-05-17_008_96.13_rf_min80pct"
     )
     basedir = run_dir.parent.parent
     overrules = []  # ["marker.roi_name=BEFL"]
     """
     # Init
+    if not run_dir.exists():
+        raise ValueError(f"run_dir does not exist: {run_dir}")
     configfiles_used = sorted((run_dir / "configfiles_used").glob("*.ini"))
+    if len(configfiles_used) == 0:
+        raise ValueError(f"No config files found in {run_dir / 'configfiles_used'}")
 
     # Check if the config files are prepended with an index. If not, raise, as the order
     # the config files are loaded is important.
@@ -50,8 +56,15 @@ def main():
 
     data_ext = conf.general["data_ext"]
     geofile_ext = conf.general["geofile_ext"]
+    output_ext = conf.general["output_ext"]
 
     input_parcel_filename = conf.calc_marker_params.getpath("input_parcel_filename")
+    parcel_path = run_dir / f"{input_parcel_filename.stem}_parcel{data_ext}"
+    input_parcel_path = input_dir / input_parcel_filename
+    input_parcel_nogeo_path = (
+        input_preprocessed_dir / f"{input_parcel_filename.stem}{data_ext}"
+    )
+
     buffer = conf.marker.getint("buffer")
     imagedata_input_parcel_filename = (
         f"{input_parcel_filename.stem}_bufm{buffer}{geofile_ext}"
@@ -72,17 +85,78 @@ def main():
     classes_refe_filename = conf.calc_marker_params.getpath("classes_refe_filename")
     classes_refe_path = refe_dir / classes_refe_filename
 
+    parcel_test_path = run_dir / f"{base_filename}_parcel_test{data_ext}"
+    parcel_predictions_proba_test_path = (
+        run_dir / f"{base_filename}_predict_proba_test{data_ext}"
+    )
     parcel_predictions_test_path = run_dir / f"{base_filename}_predict_test{data_ext}"
     parcel_predictions_test_geopath = (
         run_dir / f"{base_filename}_predict_test{geofile_ext}"
     )
-    min_parcels_in_class = conf.preprocess.getint("min_parcels_in_class")
+
+    parcel_predictions_proba_all_path = (
+        run_dir / f"{base_filename}_predict_proba_all{data_ext}"
+    )
     parcel_predictions_all_path = run_dir / f"{base_filename}_predict_all{data_ext}"
     parcel_predictions_all_geopath = (
         run_dir / f"{base_filename}_predict_all{geofile_ext}"
     )
 
-    # Run
+    min_parcels_in_class = conf.preprocess.getint("min_parcels_in_class")
+    classtype_to_prepare = conf.preprocess["classtype_to_prepare"]
+
+    # Prepare parcel input file
+    class_pre.prepare_input(
+        input_parcel_path=input_parcel_nogeo_path,
+        input_parcel_filetype=input_parcel_filetype,
+        timeseries_periodic_dir=timeseries_periodic_dir,
+        base_filename=base_filename,
+        data_ext=data_ext,
+        classtype_to_prepare=classtype_to_prepare,
+        classes_refe_path=classes_refe_path,
+        min_parcels_in_class=min_parcels_in_class,
+        output_parcel_path=parcel_path,
+    )
+
+    # Recalculate prediction consolidations
+    # -------------------------------------
+    # If it was necessary to train, there will be a test prediction... so postprocess it
+    parcel_predictions_test_path = None
+    parcel_predictions_test_geopath = None
+    if parcel_test_path is not None and parcel_predictions_proba_test_path is not None:
+        parcel_predictions_test_path = (
+            run_dir / f"{base_filename}_predict_test{data_ext}"
+        )
+        parcel_predictions_test_geopath = (
+            run_dir / f"{base_filename}_predict_test{geofile_ext}"
+        )
+        class_post.calc_top3_and_consolidation(
+            input_parcel_path=parcel_test_path,
+            input_parcel_probabilities_path=parcel_predictions_proba_test_path,
+            input_parcel_geopath=input_parcel_path,
+            output_predictions_path=parcel_predictions_test_path,
+            output_predictions_geopath=parcel_predictions_test_geopath,
+        )
+
+    # Postprocess predictions
+    parcel_predictions_all_path = run_dir / f"{base_filename}_predict_all{data_ext}"
+    parcel_predictions_all_geopath = (
+        run_dir / f"{base_filename}_predict_all{geofile_ext}"
+    )
+    parcel_predictions_all_output_path = (
+        run_dir / f"{base_filename}_predict_all_output{output_ext}"
+    )
+    class_post.calc_top3_and_consolidation(
+        input_parcel_path=parcel_path,
+        input_parcel_probabilities_path=parcel_predictions_proba_all_path,
+        input_parcel_geopath=input_parcel_path,
+        output_predictions_path=parcel_predictions_all_path,
+        output_predictions_geopath=parcel_predictions_all_geopath,
+        output_predictions_output_path=parcel_predictions_all_output_path,
+    )
+
+    # Recalculate reports
+    # -------------------
     if input_groundtruth_path is not None:
         groundtruth_path = (
             run_dir
@@ -98,6 +172,7 @@ def main():
             classes_refe_path=classes_refe_path,
             min_parcels_in_class=min_parcels_in_class,
             output_parcel_path=groundtruth_path,
+            force=True,
         )
 
     # Print full reporting on the accuracy of the test dataset
