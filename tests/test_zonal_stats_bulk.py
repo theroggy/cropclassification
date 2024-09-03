d8b3d490501fed1014921c7da5815b881aaa4ae1
KriWay <kris.vanwayenberge@lv.vlaanderen.be>
2024-09-03 10:43:47 +0200

    add importorskip for qgis

--- tests/test_zonal_stats_bulk.py
+++ tests/test_zonal_stats_bulk.py
@@ -7,6 +7,9 @@ from cropclassification.helpers import pandas_helper as pdh
 from cropclassification.util import zonal_stats_bulk
 from tests.test_helper import SampleData
 
+qgis_analysis = pytest.importorskip("qgis.analysis")
+qgis_core = pytest.importorskip("qgis.core")
+
 
 @pytest.mark.parametrize("engine", ["pyqgis", "rasterstats", "exactextract"])
 def test_zonal_stats_bulk(tmp_path, engine):
