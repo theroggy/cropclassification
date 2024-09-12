df2e8e7de96acbfb0bc1433886adbdc484b448ec
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-09-12 20:28:42 +0200

    Simplify test

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -11,18 +11,17 @@ from tests import test_helper
 
 
 @pytest.mark.parametrize(
-    "task, balancing_strategy, cross_pred_models",
+    "balancing_strategy, cross_pred_models",
     [
-        ("calc_periodic_mosaic", None, 0),
-        ("calc_marker", "BALANCING_STRATEGY_MEDIUM", 0),
-        ("calc_marker", "BALANCING_STRATEGY_MEDIUM2", 0),
-        ("calc_marker", "BALANCING_STRATEGY_PROPORTIONAL_GROUPS", 2),
-        ("calc_marker", "BALANCING_STRATEGY_UPPER_LIMIT", 0),
-        ("calc_marker", "BALANCING_STRATEGY_EQUAL", 0),
+        ("BALANCING_STRATEGY_MEDIUM", 0),
+        ("BALANCING_STRATEGY_MEDIUM2", 0),
+        ("BALANCING_STRATEGY_PROPORTIONAL_GROUPS", 2),
+        ("BALANCING_STRATEGY_UPPER_LIMIT", 0),
+        ("BALANCING_STRATEGY_EQUAL", 0),
     ],
 )
-def test_end2end_task(tmp_path, task, balancing_strategy, cross_pred_models):
-    if task == "calc_marker" and not HAS_QGIS:
+def test_end2end_task_calc_marker(tmp_path, balancing_strategy, cross_pred_models):
+    if not HAS_QGIS:
         pytest.skip("QGIS is not available on this system.")
 
     marker_basedir = tmp_path / test_helper.SampleData.marker_basedir.name
@@ -31,7 +30,7 @@ def test_end2end_task(tmp_path, task, balancing_strategy, cross_pred_models):
     # Create configparser and read task file!
     tasks_dir = marker_basedir / "_tasks"
     ignore_dir = tasks_dir / "ignore"
-    task_ini = f"task_test_{task}.ini"
+    task_ini = "task_test_calc_marker.ini"
 
     shutil.copy(src=ignore_dir / task_ini, dst=tasks_dir / task_ini)
 
@@ -40,41 +39,48 @@ def test_end2end_task(tmp_path, task, balancing_strategy, cross_pred_models):
         config_overrules=[
             f"marker.balancing_strategy={balancing_strategy}",
             f"classifier.cross_pred_models={cross_pred_models}",
+            "min_parcels_in_class=1",
         ],
     )
 
-    if task == "calc_marker":
-        run_dir = (
-            marker_basedir
-            / "2024_CROPGROUP"
-            / f"Run_{datetime.now().strftime('%Y-%m-%d')}_001"
-        )
-        assert run_dir.exists()
+    run_dir = (
+        marker_basedir / f"2024_CROPGROUP/Run_{datetime.now().strftime('%Y-%m-%d')}_001"
+    )
+    assert run_dir.exists()
+    base_stem = "Prc_BEFL_2023_2023-07-24_bufm5_weekly_predict_all"
+    if cross_pred_models <= 1:
         assert (run_dir / "model.hdf5").exists()
-        assert (
-            run_dir / "Prc_BEFL_2023_2023-07-24_bufm5_weekly_predict_all.gpkg"
-        ).exists()
-        assert (
-            run_dir
-            / "Prc_BEFL_2023_2023-07-24_bufm5_weekly_predict_all.sqlite_accuracy_report.html"  # noqa: E501
-        ).exists()
-        assert (
-            run_dir
-            / "Prc_BEFL_2023_2023-07-24_bufm5_weekly_predict_all.sqlite_accuracy_report.txt_groundtruth_pred_quality_details.gpkg"  # noqa: E501
-        )
-        df_predict = gfo.read_file(
-            path=run_dir
-            / "Prc_BEFL_2023_2023-07-24_bufm5_weekly_predict_all.sqlite_accuracy_report.txt_groundtruth_pred_quality_details.gpkg"  # noqa: E501
-        )
-        assert is_numeric_dtype(df_predict["LAYER_ID"].dtype)
-        assert is_numeric_dtype(df_predict["PRC_ID"].dtype)
-        assert is_numeric_dtype(df_predict["pixcount"].dtype)
-        assert is_numeric_dtype(df_predict["pred1_prob"].dtype)
-        assert is_numeric_dtype(df_predict["pred2_prob"].dtype)
-        assert is_numeric_dtype(df_predict["pred3_prob"].dtype)
+    assert (run_dir / f"{base_stem}.gpkg").exists()
+    assert (run_dir / f"{base_stem}.sqlite_accuracy_report.html").exists()
+    assert (
+        run_dir
+        / f"{base_stem}.sqlite_accuracy_report.txt_groundtruth_pred_quality_details.gpkg"  # noqa: E501
+    )
+    df_predict = gfo.read_file(
+        path=run_dir
+        / f"{base_stem}.sqlite_accuracy_report.txt_groundtruth_pred_quality_details.gpkg"  # noqa: E501
+    )
+    assert is_numeric_dtype(df_predict["LAYER_ID"].dtype)
+    assert is_numeric_dtype(df_predict["PRC_ID"].dtype)
+    assert is_numeric_dtype(df_predict["pixcount"].dtype)
+    assert is_numeric_dtype(df_predict["pred1_prob"].dtype)
+    assert is_numeric_dtype(df_predict["pred2_prob"].dtype)
+    assert is_numeric_dtype(df_predict["pred3_prob"].dtype)
 
-    if task == "calc_periodic_mosaic":
-        # Check if a log file was written
-        log_dir = marker_basedir / "log"
-        assert log_dir.exists()
-        assert len(list(log_dir.glob("*.log"))) == 1
+
+def test_end2end_task_calc_periodic_mosaic(tmp_path):
+    marker_basedir = tmp_path / test_helper.SampleData.marker_basedir.name
+    shutil.copytree(test_helper.SampleData.marker_basedir, marker_basedir)
+    # Create configparser and read task file!
+    tasks_dir = marker_basedir / "_tasks"
+    ignore_dir = tasks_dir / "ignore"
+    task_ini = "task_test_calc_periodic_mosaic.ini"
+
+    shutil.copy(src=ignore_dir / task_ini, dst=tasks_dir / task_ini)
+
+    cropclassification.cropclassification(tasksdir=tasks_dir)
+
+    # Check if a log file was written
+    log_dir = marker_basedir / "log"
+    assert log_dir.exists()
+    assert len(list(log_dir.glob("*.log"))) == 1
