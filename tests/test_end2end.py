680075aad93076cd4fa8ed67e0dedf411f91c0c6
KriWay <kris.vanwayenberge@lv.vlaanderen.be>
2024-09-27 12:57:07 +0200

    refactor test

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -20,10 +20,7 @@ from tests import test_helper
         ("BALANCING_STRATEGY_EQUAL", 0),
     ],
 )
-@pytest.mark.parametrize("exclude_erase_layer", [True, False])
-def test_task_calc_marker(
-    tmp_path, balancing_strategy, cross_pred_models, exclude_erase_layer
-):
+def test_task_calc_marker(tmp_path, balancing_strategy, cross_pred_models):
     if not HAS_QGIS:
         pytest.skip("QGIS is not available on this system.")
 
@@ -42,10 +39,6 @@ def test_task_calc_marker(
         f"classifier.cross_pred_models={cross_pred_models}",
         "preprocess.min_parcels_in_class=1",
     ]
-    if exclude_erase_layer:
-        config_overrules.append(
-            "calc_marker_params.erase_layer_filename=trees_19_162_BEFL-2022-2023-ofw.gpkg",
-        )
     cropclassification.cropclassification(
         tasksdir=tasks_dir,
         config_overrules=config_overrules,
