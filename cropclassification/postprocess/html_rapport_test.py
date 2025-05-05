494948fa2177780e9980d41bab3cef56a5ef300a
JoeriBroeckx <joeri.broeckx@lv.vlaanderen.be>
2025-05-05 08:34:39 +0200

    add tooltips to html df

--- cropclassification/postprocess/html_rapport_test.py
+++ cropclassification/postprocess/html_rapport_test.py
@@ -1,30 +1,40 @@
-"""Test script to generate a classification report for a test classification."""
+"""Test script to generate a classification report for a classification."""
 
 import os
 from pathlib import Path
 
 import cropclassification.postprocess.classification_reporting as class_report
+from cropclassification.helpers import config_helper as conf
 
-base_dir = Path("x:/Monitoring/Markers/PlayGround/JoeBro")
-input_dir = base_dir / "InputData"
-class_base_dir = base_dir / "HTML"
-input_groundtruth_path = input_dir / "Prc_BEFL_2018_groundTruth.csv"
-parcel_pixcount_path = input_dir / "BEFL2018_bufm10_weekly_pixcount.csv"
+base_dir = Path(r"x:/Monitoring/Markers/DEV")
+marker_dir = base_dir / r"2025_LANDCOVER-EARLY\Run_2025-04-29_001"
+input_dir = base_dir / "_inputdata"
+ts_dir = base_dir / "_ts_periodic"
+refe_dir = base_dir / "_refe"
 
-parcel_predictions_all_path = class_base_dir / "BEFL2018_bufm10_weekly_predict_all.csv"
-groundtruth_path = class_base_dir / "Prc_BEFL_2018_groundTruth_classes.csv"
-report_txt = class_base_dir / "testje_accuracy_report.txt"
+parcel_pixcount_path = (
+    input_dir / "Prc_BEFL_2025_2025-04-18_bufm5_weekly_pixcount.sqlite"
+)
+# input_groundtruth_path = input_dir / "Prc_BEFL_2018_groundTruth.csv"
+# groundtruth_path = refe_dir / "Prc_BEFL_2018_groundTruth_classes.csv"
+
+conf.read_config(Path(marker_dir / "config_used.ini"))
+
+parcel_predictions_all_path = (
+    marker_dir / "Prc_BEFL_2025_2025-04-18_bufm5_weekly_predict_all.sqlite"
+)
+report_txt = marker_dir / "test_accuracy_report.txt"
 report_html = Path(str(report_txt).replace(".txt", ".html"))
 
-if report_txt.exists():
+if os.path.exists(report_txt):
     os.remove(report_txt)
-if report_html.exists():
+if os.path.exists(report_html):
     os.remove(report_html)
 
 class_report.write_full_report(
     parcel_predictions_geopath=parcel_predictions_all_path,
     parcel_train_path=None,
     output_report_txt=report_txt,
-    parcel_ground_truth_path=groundtruth_path,
+    # parcel_ground_truth_path=groundtruth_path,
     force=True,
 )
